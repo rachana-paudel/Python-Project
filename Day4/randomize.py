4010ff165f52eb5ac031a073e8dbcd50bbb2ddc7
rachana-paudel <paudelrachana7@gmail.com>
2025-04-10 18:58:38 +0545

    generate random num

---
+++ Day4/randomize.py
@@ -0,0 +1,18 @@
+import random
+# import my_module
+
+# random_integer=random.randint(5,10)
+# print(random_integer)
+
+# print(my_module.my_favourite_num)
+
+# random_num=random.random() * 10
+# range is always 0 to 1 in random.rando()
+# multiplying with 10 give random num  from 1 to 10 only
+# print(random_num)
+
+random_head_tail=random.randint(0,1)
+if random_head_tail==0:
+    print("Head")
+else:
+    print("Tail")    
